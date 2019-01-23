405ba47f45084d8474423cd7343a5ba47fc9360c
Mauricio Andrian <andrian.mauricio@gmail.com>
2019-01-23 08:42:58 -0300

    Updated mongo uri

--- src/config.js
+++ src/config.js
@@ -47,9 +47,7 @@ const config = {
     ip: process.env.IP || undefined,
     port: process.env.PORT || 8080,
     mongo: {
-      uri:
-        process.env.MONGODB_URI ||
-        'mongodb://localhost/magnetico-coding-challenge'
+      uri: process.env.MONGODB_URI || 'mongodb://localhost:27017'
     }
   }
 }
