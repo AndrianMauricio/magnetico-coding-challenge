f58b72a5f84e7e7ed735363a04347671790ea459
Mauricio Andrian <andrian.mauricio@gmail.com>
2019-01-28 10:16:11 -0300

    Added patientStore to the PatientForm component

--- client/src/components/Form/PatientForm/index.tsx
+++ client/src/components/Form/PatientForm/index.tsx
@@ -1,42 +1,64 @@
 import './PatientForm.scss';
 
-import React from 'react';
+import { observer } from 'mobx-react';
+import React, { Component } from 'react';
 
-import { Field, Input, Select } from '../../Field';
-import { Ingredient } from '../RecipeForm/Ingredient';
+import { PatientStore } from '../../../store/patientStore';
+import { Input } from '../../Field';
 
-export const PatientForm: React.FunctionComponent = _ => (
-  <form onSubmit={e => e.preventDefault()} className="patientForm">
-    <Input id="name" type="text" label="Name" />
-    <Input id="address" type="text" label="Address" />
-    <Input id="dateOfBirth" type="date" label="Date Of Birth" />
-    <Select
-      id="preset"
-      label="Select a preset"
-      options={[
-        { value: "option1", label: "Option 1" },
-        { value: "option2", label: "Option 2" },
-        { value: "option3", label: "Option 3" },
-      ]}
-    />
-    <Field>
-      Or add an ingredient: <button type="button">+</button>
-    </Field>
-    <Ingredient
-      id="1"
-      value="15"
-      onChange={e => console.log((e.target as HTMLInputElement).value)}
-    />
-    <Ingredient
-      id="2"
-      value="15"
-      onChange={e => console.log((e.target as HTMLInputElement).value)}
-    />
-    <Ingredient
-      id="3"
-      value="15"
-      onChange={e => console.log((e.target as HTMLInputElement).value)}
-    />
-    <button type="submit">Generate PDF</button>
-  </form>
+type PatientFormProps = {
+  patientStore: PatientStore;
+};
+
+export const PatientForm = observer(
+  class PatientForm extends Component<PatientFormProps, {}> {
+    handleChangeName = (e: React.FormEvent<HTMLInputElement>) => {
+      this.props.patientStore.changeName((e.target as HTMLInputElement).value);
+    };
+
+    handleChangeAddress = (e: React.FormEvent<HTMLInputElement>) => {
+      this.props.patientStore.changeAddress(
+        (e.target as HTMLInputElement).value
+      );
+    };
+
+    handleChangeDateOfBirth = (e: React.FormEvent<HTMLInputElement>) => {
+      this.props.patientStore.changeDateOfBirth(
+        (e.target as HTMLInputElement).value
+      );
+    };
+
+    render() {
+      const { patientStore } = this.props;
+
+      return (
+        <>
+          <Input
+            id="name"
+            type="text"
+            label="Name"
+            value={patientStore.name}
+            onChange={this.handleChangeName}
+            required
+          />
+          <Input
+            id="address"
+            type="text"
+            label="Address"
+            value={patientStore.address}
+            onChange={this.handleChangeAddress}
+            required
+          />
+          <Input
+            id="dateOfBirth"
+            type="date"
+            label="Date Of Birth"
+            value={patientStore.dateOfBirth}
+            onChange={this.handleChangeDateOfBirth}
+            required
+          />
+        </>
+      );
+    }
+  }
 );
